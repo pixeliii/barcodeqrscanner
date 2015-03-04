02c568e253f5d2d3c28a24f0fd983f9ccd1d49da
Axel Andersin <axel.andersin@gmail.com>
2015-03-04 15:16:17 +0200

    Initial commit

---
+++ app/src/main/java/com/pixelidesigns/barcodeqrscanner/MainActivity.java
@@ -0,0 +1,89 @@
+package com.pixelidesigns.barcodeqrscanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Toast;
+import android.content.DialogInterface;
+
+
+
+
+public class MainActivity extends Activity {
+
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scanBar(View v) {
+        try {
+
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        }   catch (ActivityNotFoundException anfe) {
+            showDialog(MainActivity.this, "No scanner Found", "Download a scanner code activity", "Yes", "No").show();
+        }
+}
+
+
+            public void scanQR(View v) {
+                try {
+                    Intent intent = new Intent(ACTION_SCAN);
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                    startActivityForResult(intent, 0);
+                } catch (ActivityNotFoundException anfe) {
+                    showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+
+                }
+            }
+
+
+            private static AlertDialog showDialog
+                    (final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo){
+                AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+                downloadDialog.setTitle(title);
+                downloadDialog.setMessage(message);
+                downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        try {
+                            act.startActivity(intent);
+                        } catch (ActivityNotFoundException anfe){
+                        }
+                    }
+                });
+            downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialogInterface, int i) {
+                }
+                });
+                return downloadDialog.show();
+            }
+
+            public void onActivityResult(int requestCode, int resultCode, Intent intent){
+                if (requestCode== 0){
+                    if (resultCode == RESULT_OK){
+
+                        String contents = intent.getStringExtra("SCAN_RESULT");
+                        String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                        Toast toast = Toast.makeText(this, "Content:" + contents + "Format" + format, Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            }
+
+
+
+
+
+
+}
